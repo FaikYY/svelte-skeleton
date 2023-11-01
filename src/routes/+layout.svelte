00b329803c38db2edb6175a9691c5f2f49c196d1
FaikYY <yazilimcifaik@gmail.com>
2023-10-31 20:51:03 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,87 @@
+<script>
+</script>
+
+<div class="container">
+	<header>
+		<div id="logo">Faik Yesilyaprak</div>
+
+		<nav>
+			<a href="#"> Home </a>
+
+			<a href="#"> Projects </a>
+
+			<a href="#"> About Me </a>
+
+			<a href="#"> FAQs </a>
+		</nav>
+	</header>
+
+	<main>
+		<slot />
+		<footer>Created by Faik Yesilyaprak with ❤️ 🧠</footer>
+	</main>
+</div>
+
+<style>
+	.container {
+		/* background: red; */
+		gap: calc(16px + 1vw);
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-template-rows: calc(7.5vh + 7.5vw) 1fr;
+		width: 100vw;
+		height: 100%;
+		min-height: 100%;
+		box-sizing: border-box;
+		overflow-x: hidden;
+	}
+
+	header {
+		background-color: white;
+		grid-column: 2/12;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+
+		display: flex;
+		flex-direction: column;
+
+		position: sticky;
+		top: 0;
+	}
+
+	nav {
+		/*   background: purple; */
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	a {
+		font-weight: 700;
+	}
+
+	main {
+		/* background: blue; */
+		grid-column: 2/12;
+	}
+
+	#logo {
+		/* background: yellow; */
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		align-items: center;
+		font-size: 2rem;
+	}
+
+	footer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 1rem;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<h1>Welcome</h1>
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo, dignissimos ea! Obcaecati
+	excepturi, neque ducimus repellendus illo nisi fugit dolore consequatur a facilis natus ex
+	pariatur nihil nobis deleniti modi. Error nesciunt nam velit distinctio ipsa quas cupiditate
+	expedita reprehenderit autem tempora enim ex dolorum cumque cum quis ipsum vero minus nihil quia,
+	mollitia, quisquam quibusdam facere natus iste. Optio maxime dolore dignissimos. Soluta harum
+	voluptatem rem quia exercitationem omnis possimus natus, voluptatum quisquam incidunt consequuntur
+	praesentium a aperiam earum at aliquid provident eveniet fugit laudantium! Nam laborum, culpa
+	doloribus accusantium obcaecati voluptates harum consequuntur maiores voluptas officia dignissimos
+	aspernatur, quas porro! Voluptate illum quaerat explicabo, rem dignissimos nam, quisquam
+	doloremque numquam, dolores dicta corrupti. Sint repellat ullam pariatur animi error? Suscipit
+	ipsa amet blanditiis, sint, eaque dolor ex ut voluptatum temporibus deleniti odio maxime! Velit
+	accusamus ad soluta odit hic iure non eaque quasi aut laudantium est in repellat at nihil
+	consequuntur animi autem accusantium voluptates saepe, odio deleniti nulla cumque deserunt!
+	Exercitationem fugiat excepturi harum necessitatibus ratione laudantium accusamus maiores minima
+	aut explicabo! Atque quisquam, autem commodi animi maiores aliquam facere illum rem obcaecati
+	veritatis, ea temporibus soluta qui? In voluptatem, placeat qui deleniti nesciunt velit rem
+	distinctio, cumque amet asperiores corporis, sunt vitae nemo. Velit placeat molestiae asperiores
+	nostrum aut unde veniam dignissimos consequuntur. Aperiam dignissimos aspernatur sunt sint in odio
+	ipsam corporis hic expedita nemo officia, dolorum voluptas, illum architecto ipsum quas vel
+	maiores voluptatum labore iusto ducimus libero assumenda. Commodi, est voluptatibus? Illum
+	deleniti rem obcaecati odio sed vero error eos minima? Tempora, eaque ad. Molestiae ipsam non
+	atque, voluptates quas mollitia quisquam. Quo, ut eius. Non aut quidem ex quo accusantium
+	voluptatum porro ullam illo velit aperiam dolorum sed neque fugiat illum obcaecati rerum quas, eum
+	debitis officiis? Molestias quo inventore aliquid et ullam.
+</p>
